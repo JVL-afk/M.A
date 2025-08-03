e35bf169139a1e81585b830f70d5f89f94cde1eb
JVL-afk <andreimiroiu2019@gmail.com>
2025-08-03 18:34:54 +0300

    Update route.ts

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { connectToDatabase } from '../../../../../lib/mongodb';
+import { connectToDatabase } from '../../../../lib/mongodb';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
